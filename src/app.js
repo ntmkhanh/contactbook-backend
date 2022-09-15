2293a8f2f9142ecb85c489ecac63586a22a6f9a9
My Khanh Nguyen <106447791+ntmkhanh@users.noreply.github.com>
2022-09-15 10:04:17 +0700

    Implement an error handling middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const contactController = require('./controllers/contact.controller');
 
 const app = express();
 
@@ -9,4 +10,12 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.json({message: `Welcome to contact book application.`});
 });
+
+app.route('/api/contacts/favorite').get(contactController.findAllFavorite);
+
+app.route('api/contacts/:id')
+    .get(contactController.findOne)
+    .put(contactController.update)
+    .delete(contactController.delete);
+
 module.exports = app;
